182498e4ff1549690a6559975ada5dff2c342242
Victor Montanez <victormontanez@Victors-MacBook-Pro.local>
2020-03-14 17:36:08 -0400

    added byte

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -3,8 +3,6 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-	int myAge =30;
-	int herAge = myAge;
-	System.out.println(herAge);
+	byte age = 30;
     }
 }
