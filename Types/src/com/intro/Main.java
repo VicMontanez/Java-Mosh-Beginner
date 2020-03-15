d2634fcf61e58782a8b69cd2037ae031c9b27373
VicMontanez <v_montanez@hotmail.com>
2020-03-14 20:17:09 -0400

    system out

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	byte age = 30;
-	long viewsCount = 3_123_456_789L;
-	float price = 10.99F;
-	char letter = 'A';
-	boolean isEligible = false;
+	System.out.println(age);
+
+	
     }
 }
