5dd9621d4402f461f8ccef176f8e18458574431f
VicMontanez <v_montanez@hotmail.com>
2020-03-14 21:24:48 -0400

    reference type

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -1,14 +1,14 @@
 package com.intro;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-	byte x = 1;
-	byte y = x;
-	x = 2;
-	System.out.println(y);
-
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
