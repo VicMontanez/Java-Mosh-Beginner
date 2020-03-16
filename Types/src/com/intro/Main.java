2972a6fe88211e2913f03295fab4028fb313bc0d
VicMontanez <v_montanez@hotmail.com>
2020-03-15 21:58:08 -0400

    ends with boolean

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
       String message = "Hello World" + "!!";
-      System.out.println(message);
+
+      System.out.println(message.endsWith("!!"));
     }
 }
