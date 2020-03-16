da66a976e2841653718a252c260b9617e854f311
VicMontanez <v_montanez@hotmail.com>
2020-03-15 22:12:46 -0400

    used backslash escape sequence

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -6,8 +6,8 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-      String message = "Hello World" + "!!";
+      String message = "Hello \"Mosh\"";
 
-      System.out.println(message.toLowerCase());
+      System.out.println(message);
     }
 }
