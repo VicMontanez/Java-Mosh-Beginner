344af967b4c44603bee734e69644ee30ca5f1071
VicMontanez <v_montanez@hotmail.com>
2020-03-15 23:21:11 -0400

    casting

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -5,7 +5,9 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        final float PI = 3.14F;
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
+        System.out.println(y);
 
 
     }
