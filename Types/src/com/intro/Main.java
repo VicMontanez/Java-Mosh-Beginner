3bcf31e590db0a2ae27f45b2b9eb12f5f8dc9070
VicMontanez <v_montanez@hotmail.com>
2020-03-15 23:28:38 -0400

    math classes

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -5,9 +5,14 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        String x = "1.1";
-        double y = Double.parseDouble(x) + 2;
-        System.out.println(y);
+        //int result = Math.round(1.1F);
+        //int result = (int)Math.ceil(1.1F);
+        //int result = (int)Math.floor(1.1F);
+        //int result = Math.max(1, 2);
+        //double result = Math.random() * 100;
+        int result = (int) Math.round(Math.random() * 100);
+        System.out.println(result);
+
 
 
     }
