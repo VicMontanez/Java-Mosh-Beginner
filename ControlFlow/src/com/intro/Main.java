4b9ceef58b2b46808b8e158b37d55a02dd22b555
VicMontanez <v_montanez@hotmail.com>
2020-03-17 18:15:21 -0400

    logicial operators

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -3,8 +3,15 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
-        int y = 1;
-        System.out.println(x <= y);
+        //&& 'and' operator
+//       int temperature = 22;
+//       boolean isWarm = temperature > 20 && temperature < 30;
+//       System.out.println(isWarm);
+
+        //|| 'Or/ operator
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord; //! 'not' operator
     }
 }
