0fc51ead0c9cec572ce5499701f75f977f351b89
VicMontanez <v_montanez@hotmail.com>
2020-03-17 00:08:37 -0400

    made it possible to input without making new line

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
         byte age = scanner.nextByte();
         System.out.println("You are " + age);
 
