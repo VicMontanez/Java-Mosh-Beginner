4b29d8103f4a37fd89c492f67671f69431dba94b
VicMontanez <v_montanez@hotmail.com>
2020-03-17 15:09:36 -0400

    take in user input for prinicpal

---
+++ Types/MortgageCalculator/src/com/intro/Main.java
@@ -0,0 +1,13 @@
+package com.intro;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+    }
+}
