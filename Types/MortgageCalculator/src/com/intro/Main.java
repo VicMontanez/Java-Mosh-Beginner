d9450c8ee8381551030023c5c91b2e55cd2ad919
VicMontanez <v_montanez@hotmail.com>
2020-03-17 15:36:25 -0400

    completed mortgage calculator

--- Types/MortgageCalculator/src/com/intro/Main.java
+++ Types/MortgageCalculator/src/com/intro/Main.java
@@ -1,11 +1,13 @@
 package com.intro;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
         System.out.print("Interest Rate: ");
@@ -19,7 +21,8 @@ public class Main {
 
         double mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate, paymentNumber) / (Math.pow(1 + monthlyRate, paymentNumber) - 1));
 
-        System.out.println(mortgage);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format((mortgage));
+        System.out.println("Mortgage is: " + mortgageFormatted);
 
     }
 }
