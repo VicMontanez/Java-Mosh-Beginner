6c0776a7b7bdd0c53818b4d9d24e473a3406df35
VicMontanez <v_montanez@hotmail.com>
2020-03-17 00:09:34 -0400

    made it print inputted name

--- Types/src/com/intro/Main.java
+++ Types/src/com/intro/Main.java
@@ -8,9 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Age: ");
-        byte age = scanner.nextByte();
-        System.out.println("You are " + age);
+        System.out.print("Name: ");
+        String name = scanner.next();
+        System.out.println("You are " + name);
 
 
 
