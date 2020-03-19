7ae6f16c5c16fda00c090dbc0c0149883929c022
VicMontanez <v_montanez@hotmail.com>
2020-03-19 00:09:46 -0400

    print out user number

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (input.equals("quit")) {
+        while (!input.equals("quit")) {
             System.out.print("Input: ");
-            ;
             input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
        }
 
