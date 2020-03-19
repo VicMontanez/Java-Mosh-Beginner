c3510a297c174374ad045b8038b3645b792fee1f
VicMontanez <v_montanez@hotmail.com>
2020-03-19 00:28:50 -0400

    for each loop

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -5,17 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-        while (true) {
-            System.out.print("Input: ");
-            input = scanner.next().toLowerCase();
-            if (input.equals("pass"))
-                continue;
-            if (input.equals("quit"))
-                break;
-            System.out.println(input);
-        }
+        String[] fruits = {"Apple", "Mango", "Orange"
+        };
+
+        for (int i = fruits.length; i > 0; i--)
+            System.out.println(fruits[i]);
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
 
        }
 
