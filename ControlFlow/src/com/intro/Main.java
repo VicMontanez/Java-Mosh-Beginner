2a22473f4e6c33f9dce6bb36e197b7b190f9cf3c
VicMontanez <v_montanez@hotmail.com>
2020-03-18 22:16:20 -0400

    simplify if statements

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -3,14 +3,8 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-    int temp = 32;
-    if (temp > 30) {
-        System.out.println("It's a hot day");
-        System.out.println("Drink water");
-    }
-    else if (temp > 20)
-        System.out.println("Beautiful day");
-    else
-        System.out.print("Cold day");
+        int income = 120_000;
+        boolean hasHighIncome = income > 100_000;
+
     }
 }
