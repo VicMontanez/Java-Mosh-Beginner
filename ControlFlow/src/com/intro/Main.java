f8dc60f8f0853bf3c95bb17461063a1fb3d1edd2
VicMontanez <v_montanez@hotmail.com>
2020-03-19 00:22:19 -0400

    switched to true

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.print("Input: ");
             input = scanner.next().toLowerCase();
             if (input.equals("pass"))
