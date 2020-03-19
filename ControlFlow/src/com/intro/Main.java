bade1a9eb2e283b85ab5c8213cf147ee701dda7f
VicMontanez <v_montanez@hotmail.com>
2020-03-19 00:03:01 -0400

    while loop

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -3,9 +3,12 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-       for (int i = 0; i < 5; i++)
-           System.out.println("Hello World"+ i) ;
 
+       int i = 0;
+       while (i >0){
+           System.out.println("Hello World"+ i);
+           i--;
+       }
 
 
     }
