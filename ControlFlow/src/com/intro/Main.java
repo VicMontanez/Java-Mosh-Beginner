58d8bef9b76116ccdf58487bd321cfd377514601
VicMontanez <v_montanez@hotmail.com>
2020-03-18 22:39:25 -0400

    ternary operator

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         int income = 120_000;
-        boolean hasHighIncome = (income > 100_000);
+        String className = income > 100_000 ? "First" : "Economy";
+
 
     }
 }
