705d8cd4905af0b1da1ff112bedbfd892b4dd54d
VicMontanez <v_montanez@hotmail.com>
2020-03-19 00:06:49 -0400

    make user input to lowercase

--- ControlFlow/src/com/intro/Main.java
+++ ControlFlow/src/com/intro/Main.java
@@ -1,15 +1,19 @@
 package com.intro;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-       int i = 0;
-       while (i >0){
-           System.out.println("Hello World"+ i);
-           i--;
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (input.equals("quit")) {
+            System.out.print("Input: ");
+            ;
+            input = scanner.next().toLowerCase();
+        }
        }
 
 
     }
-}
+
