9f0a2ba5d2827aff0e745140354b965dfae82932
VicMontanez <v_montanez@hotmail.com>
2020-03-19 23:11:35 -0400

    completed mortgage calculator

--- ControlFlow/MortgageCalculator2/src/com/intro/Main.java
+++ ControlFlow/MortgageCalculator2/src/com/intro/Main.java
@@ -1,5 +1,6 @@
 package com.intro;
 
+import javax.swing.plaf.IconUIResource;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -9,35 +10,45 @@ public class Main {
     //Years  value between 1 and 30
     public static void main(String[] args) {
 
-        int principal = 0;
+
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Principal: ");
+            int principal = scanner.nextInt();
+            if (principal < 1000 || principal > 1000000) {
+                System.out.println("Enter a value between 1K - 1M");
+                continue;
+            }
             while (true) {
-                System.out.print("Principal: ");
-                principal = scanner.nextInt();
-                if (principal < 1000 || principal > 1000000)
-                    System.out.println("Enter a value between 1K - 1M");
-                break;
+                System.out.print("Interest Rate: ");
+                float rate = scanner.nextFloat();
+                if (rate < 0 || rate > 30) {
+                    System.out.println("Enter a value between 1 - 30");
+                    continue;
+                }
+                while (true) {
+                    System.out.print("Length of loan (Years): ");
+                    int years = scanner.nextInt();
+                    if (years < 1 || years > 30) {
+                        System.out.println("Enter a value between 1 - 30");
+                        continue;
+                    }
+
+                    int paymentNumber = 12 * years;
+
+                    float monthlyRate = rate / 100 / 12;
+
+                    double mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate, paymentNumber) / (Math.pow(1 + monthlyRate, paymentNumber) - 1));
+
+                    String mortgageFormatted = NumberFormat.getCurrencyInstance().format((mortgage));
+                    System.out.println("Mortgage is: " + mortgageFormatted);
+                    break;
+                }
+              break;
             }
-            System.out.print("Interest Rate: ");
-            float rate = scanner.nextFloat();
-            if (rate < 0 || rate > 30)
-                System.out.println("Enter a value between 1 - 30");
-            System.out.print("Length of loan (Years): ");
-            int years = scanner.nextInt();
-            if (years < 1 || years > 30) {
-                System.out.println("Enter a value between 1 and 30");
-
-            }
-
-            int paymentNumber = 12 * years;
-
-            float monthlyRate = rate / 100 / 12;
-
-            double mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate, paymentNumber) / (Math.pow(1 + monthlyRate, paymentNumber) - 1));
-
-            String mortgageFormatted = NumberFormat.getCurrencyInstance().format((mortgage));
-            System.out.println("Mortgage is: " + mortgageFormatted);
+            break;
 
         }
     }
+}
