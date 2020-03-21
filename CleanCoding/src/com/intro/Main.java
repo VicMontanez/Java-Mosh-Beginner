e983c40fd964ace9a99686764a85b10df3e0f2a5
VicMontanez <v_montanez@hotmail.com>
2020-03-20 23:09:39 -0400

    added second parameter to method

--- CleanCoding/src/com/intro/Main.java
+++ CleanCoding/src/com/intro/Main.java
@@ -3,12 +3,12 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        greetUser("Mosh");
-        greetUser("John");
+
+
 
     }
 
-    public static void greetUser(String name){
-        System.out.println("Hello" + name);
+    public static void greetUser(String firstName, String lastName){
+        System.out.println("Hello" + firstName + " " + lastName);
     }
 }
