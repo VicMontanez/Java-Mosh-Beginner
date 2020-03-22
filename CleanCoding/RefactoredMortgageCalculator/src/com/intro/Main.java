a4edfcee82103a13d9b8e062958d7a2a4f39b4ba
VicMontanez <v_montanez@hotmail.com>
2020-03-21 20:25:45 -0400

    refactoring repetitive patterns

--- CleanCoding/RefactoredMortgageCalculator/src/com/intro/Main.java
+++ CleanCoding/RefactoredMortgageCalculator/src/com/intro/Main.java
@@ -8,44 +8,32 @@ public class Main {
     //Interest rate greater than 0 or less than or equal to 30
     //Years  value between 1 and 30
     public static void main(String[] args) {
+        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+        float rate = (float)readNumber("Annual Interest Rate: ", 1, 30);
+        int years = (int)readNumber("Period (Years): ", 1, 30);
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Principal: ");
-            int principal = scanner.nextInt();
-            if (principal < 1000 || principal > 1000000) {
-                System.out.println("Enter a value between 1K - 1M");
-                continue;
-            }
-            while (true) {
-                System.out.print("Interest Rate: ");
-                float rate = scanner.nextFloat();
-                if (rate < 0 || rate > 30) {
-                    System.out.println("Enter a value between 1 - 30");
-                    continue;
-                }
-                while (true) {
-                    System.out.print("Length of loan (Years): ");
-                    int years = scanner.nextInt();
-                    if (years < 1 || years > 30) {
-                        System.out.println("Enter a value between 1 - 30");
-                        continue;
-                    }
 
 
                     double mortgage = calculateMortgage(principal, rate, years);
 
                     String mortgageFormatted = NumberFormat.getCurrencyInstance().format((mortgage));
                     System.out.println("Mortgage is: " + mortgageFormatted);
-                    break;
-                }
-                break;
-            }
-            break;
 
-        }
+                }
+
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print("Interest Rate: ");
+            value = scanner.nextFloat();
+            if (value < min || value > max) {
+                System.out.println("Enter a value between " + min + " and " + max);
+                continue;
+            }
+
+            return value;
     }
 
     public static double calculateMortgage(int principal,
